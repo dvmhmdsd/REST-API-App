cbfd780451072ee446e5b528de102f18999bd07e
mohamed-saad <mahameds19999@gmail.com>
2019-01-10 20:23:03 +0200

    add the geoJSON to the hero model

--- models/hero.js
+++ models/hero.js
@@ -1,6 +1,19 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
+// geoJSON
+let geoSchema = new Schema({
+    type: {
+        type: String,
+        default: 'Point'
+    },
+    coordinates: {
+        type: [Number],
+        index: '2dsphere'
+    }
+});
+
+
 let heroSchema = new Schema({
     name: {
         type: String,
@@ -12,7 +25,8 @@ let heroSchema = new Schema({
     available: {
         type: Boolean,
         default: false
-    }
+    },
+    geometry: geoSchema
 });
 
 let Hero = mongoose.model('Hero', heroSchema);
